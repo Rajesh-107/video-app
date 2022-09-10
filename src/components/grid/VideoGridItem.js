0329db8307a10a4fee206298dea422faf46e585c
Rajesh-107 <applerb326@gmail.com>
2022-09-10 22:03:29 +0600

    show all data in videogrid item

--- src/components/grid/VideoGridItem.js
+++ src/components/grid/VideoGridItem.js
@@ -6,40 +6,40 @@ const VideoGridItem = ({video}) => {
     <div class="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div class="w-full flex flex-col">
         <div class="relative">
-          <Link to="/videos/1" href="video.html">
+          <Link to={`videos/${video.id}`} >
             <img
-              src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
+              src={video.thumbnail}
               class="w-full h-auto"
-              alt="Some video title"
+              alt={video.title}
             />
           </Link>
 
           <p class="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-            12:10
+            {video.duration}
           </p>
         </div>
 
         <div class="flex flex-row mt-2 gap-2">
-          <Link to="/videos/1" class="shrink-0">
+          <Link to={`videos/${video.id}`} class="shrink-0">
             <img
-              src="https://avatars.githubusercontent.com/u/73503432?v=4"
+              src={video.avatar}
               class="rounded-full h-6 w-6"
-              alt="Learn with Sumit"
+              alt={video.author}
             />
           </Link>
 
           <div clas="flex flex-col">
-            <Link to="/videos/1">
-              <p class="text-slate-900 text-sm font-semibold">Video title</p>
+            <Link to={`videos/${video.id}`}>
+              <p class="text-slate-900 text-sm font-semibold">{video.title}</p>
             </Link>
             <Link
-              to="/videos/1"
+              to={`videos/${video.id}`}
               class="text-gray-400 text-xs mt-2 hover:text-gray-600"
               href="#"
             >
-              Learn with Sumit
+             {video.author}
             </Link>
-            <p class="text-gray-400 text-xs mt-1">200 views . May 3, 2022</p>
+            <p class="text-gray-400 text-xs mt-1">{video.views} . {video.date}</p>
           </div>
         </div>
       </div>
