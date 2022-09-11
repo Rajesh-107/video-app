c7e1090c842076fdfc9e57da782eaa544101d764
Rajesh-107 <applerb326@gmail.com>
2022-09-12 01:20:06 +0600

    filter and search implment

--- src/features/videos/videosAPI.js
+++ src/features/videos/videosAPI.js
@@ -1,8 +1,17 @@
 import axios from "../../utils/axios"
 
 
-export const getVideos = async () => {
-    const res = await axios.get('/videos');
+export const getVideos = async (tags, search) => {
+    let queryString = '';
+
+    if(tags?.length > 0){
+        queryString += tags.map(tag => `tag_like=${tag}`).join("&");
+    }
+    if(search !== ""){
+        queryString += `&q=${search}`;
+    }
+
+    const res = await axios.get(`/videos/?${queryString}`);
     
     return res.data;
 }
